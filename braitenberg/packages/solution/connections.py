760df03dbc4aaf60c128a63d747004b99b68dee0
SalmaSaifi <saifi.salma10@gmail.com>
2026-01-12 15:44:56 +0100

    feat implement braitenberg

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -2,7 +2,7 @@ from typing import Tuple
 
 import numpy as np
 
-
+"""
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
@@ -19,4 +19,42 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     # these are random values
     res[100:150, 100:300] = -1
     # ---
+    return res
+"""
+
+def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
+    H, W = shape
+    res = np.zeros(shape, dtype=np.float32)
+    
+    near_zone = int(H * 0.5)  # Moitié inférieure = zone proche
+    mid_w = W // 2            # Centre horizontal
+    
+    # Zone haute : avancer
+    res[:near_zone, :] = 1.0
+    
+    # Zone basse gauche : légèrement avancer
+    res[near_zone:, :mid_w] = 0.5
+    
+    # Zone basse droite : tourner à gauche (freiner moteur gauche)
+    res[near_zone:, mid_w:] = -1.0
+    
+    return res
+
+
+def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
+    H, W = shape
+    res = np.zeros(shape, dtype=np.float32)
+    
+    near_zone = int(H * 0.5)
+    mid_w = W // 2
+    
+    # Zone haute : avancer
+    res[:near_zone, :] = 1.0
+    
+    # Zone basse droite : légèrement avancer
+    res[near_zone:, mid_w:] = 0.5
+    
+    # Zone basse gauche : tourner à droite (freiner moteur droit)
+    res[near_zone:, :mid_w] = -1.0
+    
     return res
--- braitenberg/packages/solution/preprocessing.py
+++ braitenberg/packages/solution/preprocessing.py
@@ -1,9 +1,18 @@
 import cv2
 import numpy as np
 
-lower_hsv = np.array([171, 140, 100])
-upper_hsv = np.array([179, 200, 255])
+#lower_hsv = np.array([171, 140, 100])
+#upper_hsv = np.array([179, 200, 255])
+#lower_hsv = np.array([0, 40, 60])
+#upper_hsv = np.array([25, 180, 255])
 
+#lower_hsv = np.array([0, 0, 0])
+#upper_hsv = np.array([100, 255, 255])
+#lower_hsv = np.array([15, 100, 100])  
+#upper_hsv = np.array([35, 255, 255])
+
+lower_hsv = np.array([10, 80, 80])    
+upper_hsv = np.array([40, 255, 255])
 
 def preprocess(image_rgb: np.ndarray) -> np.ndarray:
     """Returns a 2D array"""
